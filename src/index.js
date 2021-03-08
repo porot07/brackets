12b636d12ad487302c7e0f6d0cc526900b5c6821
porot07 <ciscomalware@github.com>
2021-03-08 17:48:39 +0500

    feat: implement functional check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const result = [str[0]];
+
+    const dictionaryBrackets = bracketsConfig.reduce((acc, [open, close]) => ({ ...acc, [open]: close }), {});
+    
+    for (let cnt = 1; cnt < str.length; cnt += 1) {
+      if (str[cnt] === dictionaryBrackets[result[result.length - 1]]) {
+        result.pop();
+      } else {
+        result.push(str[cnt]);
+      }
+    }
+
+    return result.length >= 1 ? false : true;
+};
